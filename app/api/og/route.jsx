1a32648842bca57b2b45186ddd68ba36c42bece8
csksn1jaejoon888 <csksn1.jaejoon888@gmail.com>
2026-05-19 23:47:02 +0800

    Update route.jsx

--- app/api/og/route.jsx
+++ app/api/og/route.jsx
@@ -30,20 +30,31 @@ export async function GET(request) {
 
   return new ImageResponse(
 
-    (
-      <div
+  (
+    <div
+      style={{
+        width: '1200px',
+        height: '630px',
+        background: '#111',
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative',
+        color: '#fff',
+        padding: '60px',
+        fontFamily: 'sans-serif',
+      }}
+    >
+
+      <img
+        src="https://raw.githubusercontent.com/csksn1jaejoon888/og-generator/main/app/api/og/bg.png"
+        width="1200"
+        height="630"
         style={{
-          width: '1200px',
-          height: '630px',
-          background: '#111',
-          display: 'flex',
-          flexDirection: 'column',
-          position: 'relative',
-          color: '#fff',
-          padding: '60px',
-          fontFamily: 'sans-serif',
+          position: 'absolute',
+          top: 0,
+          left: 0,
         }}
-      >
+      />
 
         {/* LOGO */}
         <div
